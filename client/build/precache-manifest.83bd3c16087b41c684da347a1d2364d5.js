259c826f5a469956ad76af636506d0eac4aec1b2
Sal's Trash Can <odhamtrashcan5@Sals-Mac-Pro.local>
2019-08-20 14:53:18 -0400

    again

---
+++ client/build/precache-manifest.83bd3c16087b41c684da347a1d2364d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db49d14adcf68146594e",
+    "url": "/static/css/main.07cc139c.chunk.css"
+  },
+  {
+    "revision": "db49d14adcf68146594e",
+    "url": "/static/js/main.db49d14a.chunk.js"
+  },
+  {
+    "revision": "c4218ec7806cff9d459b",
+    "url": "/static/js/1.c4218ec7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "a02da14a238e2fe8c4adbb5495c4b374",
+    "url": "/index.html"
+  }
+];
